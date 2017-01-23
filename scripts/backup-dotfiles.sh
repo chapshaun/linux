bb44d2e07d40ab73d5da073f8bac34529d33ddbb
chapshaun <shaun.chun.chap@gmail.com>
2017-01-23 16:53:24 -0700

    ranger backup fix, new script

--- scripts/backup-dotfiles.sh
+++ scripts/backup-dotfiles.sh
@@ -11,24 +11,24 @@
 # DESC: this will backup all my dot files to github
 
 # DIRs
-cp -r ~/.conkysetups ~/GitHub/linux/backups/dotfiles/
-cp -r ~/.icons ~/GitHub/linux/backups/dotfiles/
+cp -r ~/.conkysetups ~/GitHub/linux/dotfiles/
+cp -r ~/.icons ~/GitHub/linux/dotfiles/
 # Config DIR
-cp -r ~/.config/ranger/ ~/GitHub/linux/backups/dotfiles/
-cp -r ~/.config/htop/ ~/GitHub/linux/backups/dotfiles/
-cp -r ~/.config/plank/ ~/GitHub/linux/backups/dotfiles/
-cp -r ~/.config/viewnior/ ~/GitHub/linux/backups/dotfiles/
+cp -r ~/.config/htop/ ~/GitHub/linux/dotfiles/
+cp -r ~/.config/plank/ ~/GitHub/linux/dotfiles/
+cp -r ~/.config/viewnior/ ~/GitHub/linux/dotfiles/
 
 # FILEs
-cp ~/.bashrc ~/GitHub/linux/backups/dotfiles/
-cp ~/.tmux.conf ~/GitHub/linux/backups/dotfiles/
-cp ~/.tmux-session ~/GitHub/linux/backups/dotfiles/
-cp ~/.conkyrc ~/GitHub/linux/backups/dotfiles/
-cp ~/.gitconfig ~/GitHub/linux/backups/dotfiles/
-cp ~/.vimrc ~/GitHub/linux/backups/dotfiles/
-cp ~/.face ~/GitHub/linux/backups/dotfiles/
+cp ~/.bashrc ~/GitHub/linux/dotfiles/
+cp ~/.tmux.conf ~/GitHub/linux/dotfiles/
+cp ~/.tmux-session ~/GitHub/linux/dotfiles/
+cp ~/.conkyrc ~/GitHub/linux/dotfiles/
+cp ~/.gitconfig ~/GitHub/linux/dotfiles/
+cp ~/.vimrc ~/GitHub/linux/dotfiles/
+cp ~/.face ~/GitHub/linux/dotfiles/
+cp ~/.config/ranger/rc.conf ~/GitHub/linux/dotfiles/
+cp ~/.config/ranger/rifle.conf ~/GitHub/linux/dotfiles/
 # Config Files
-cp ~/.config/tear_test.mp4 ~/GitHub/linux/backups/dotfiles/
-cp ~/.config/kid3rc ~/GitHub/linux/backups/dotfiles/
-cp ~/.config/compton.conf ~/GitHub/linux/backups/dotfiles/
-
+cp ~/.config/tear_test.mp4 ~/GitHub/linux/dotfiles/
+cp ~/.config/kid3rc ~/GitHub/linux/dotfiles/
+cp ~/.config/compton.conf ~/GitHub/linux/dotfiles/
