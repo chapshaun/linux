43ba655a676b0caf232380ecba987e4b460bfc91
chapshaun <shaun.chun.chap@gmail.com>
2017-01-26 22:14:44 -0700

    added franz script

---
+++ scripts/franz.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#        __                     __                    
+#  _____/ /_  ____ _____  _____/ /_  ____ ___  ______ 
+# / ___/ __ \/ __ `/ __ \/ ___/ __ \/ __ `/ / / / __ \
+#/ /__/ / / / /_/ / /_/ (__  ) / / / /_/ / /_/ / / / /
+#\___/_/ /_/\__,_/ .___/____/_/ /_/\__,_/\__,_/_/ /_/ 
+#               /_/                                   
+#		https://github.com/chapshaun
+#		https://twitter.com/harimakito
+
+# DESC: open franz messenger
+
+~/Programs/Franz-linux-x64-4.0.4/Franz
