5af09082dd73fa66dbe2e132ede0badeb07fdf15
chapshaun <shaun.chun.chap@gmail.com>
2018-11-10 10:36:38 -0700

    updated mac update script

--- osx/scripts/update.sh
+++ osx/scripts/update.sh
@@ -1,1 +1,15 @@
-brew update && brew upgrade
+#!/usr/bin/zsh
+#        __                     __                    
+#  _____/ /_  ____ _____  _____/ /_  ____ ___  ______ 
+# / ___/ __ \/ __ `/ __ \/ ___/ __ \/ __ `/ / / / __ \
+#/ /__/ / / / /_/ / /_/ (__  ) / / / /_/ / /_/ / / / /
+#\___/_/ /_/\__,_/ .___/____/_/ /_/\__,_/\__,_/_/ /_/ 
+#               /_/                                   
+#		https://github.com/chapshaun
+#		https://twitter.com/harimakito
+
+# DESC: quick update script
+
+# Homebrew
+brew update
+# Proceed to individually upgrade single packages
