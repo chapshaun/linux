fd2eb4228eb62684a02e4ef21923ab3bb0b50fea
Shaun Chap <shaun.chun.chap@gmail.com>
2019-02-25 12:52:00 -0700

    Update ffmpeg.sh

--- notes/programs/ffmpeg.sh
+++ notes/programs/ffmpeg.sh
@@ -32,3 +32,7 @@ ffmpeg -i 'video.mp4' -i 'audio.mp3' -codec copy -shortest 'output.mp4'
 ffmpeg -i input1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts intermediate1.ts
 ffmpeg -i input2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts intermediate2.ts
 ffmpeg -i "concat:intermediate1.ts|intermediate2.ts" -c copy -bsf:a aac_adtstoasc output.mp4
+
+
+#certain cut certain times in videos
+ffmpeg -ss 00:01:00 -i input.mp4 -to 00:02:00 -c copy output.mp4
