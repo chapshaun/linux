c533853f51025fda62839c7d5f567a20e592356c
chapshaun <shaun.chun.chap@gmail.com>
2019-11-27 01:40:39 -0700

    added pixiv.sh

---
+++ scripts/pixiv.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/zsh
+#        __                     __                    
+#  _____/ /_  ____ _____  _____/ /_  ____ ___  ______ 
+# / ___/ __ \/ __ `/ __ \/ ___/ __ \/ __ `/ / / / __ \
+#/ /__/ / / / /_/ / /_/ (__  ) / / / /_/ / /_/ / / / /
+#\___/_/ /_/\__,_/ .___/____/_/ /_/\__,_/\__,_/_/ /_/ 
+#               /_/                                   
+#		https://github.com/chapshaun
+#		https://twitter.com/harimakito
+
+## DESC: 
+## REQUIREMENTS:
+
+echo "Pixiv/Deviant art? [y/N]";
+read ANS;
+if [  "$ANS" = "Y" ] || [  "$ANS" = "y" ]
+then
+    echo "Enter Username, then Password: " ;
+    read USER;
+    read PASS;
+    notify-send -i " " 'Download Starting ...' -t 5000
+    gallery-dl -u $USER -p $PASS $1;
+    notify-send -i " " 'Download Finished ...' -t 5000
+else
+    notify-send -i " " 'Download Starting ...' -t 5000
+    gallery-dl $1;
+    notify-send -i " " 'Download Finished ...' -t 5000
+fi
+
